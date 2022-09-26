7a4bcff45d2f5cedf2f8ab0c2ecac30dc0a8a447
Arkan Mahardika <114378553+arkan3h@users.noreply.github.com>
2022-09-26 18:06:39 +0700

    Add files via upload

---
+++ C#/Polymorphism/CSPBO_2_3/CSPBO_2_3/Program.cs
@@ -0,0 +1,112 @@
+﻿class BangunDatar
+{
+    public virtual float luas()
+    {
+        Console.WriteLine("\nMenghitung laus bangun datar");
+        return 0;
+    }
+    public virtual float keliling()
+    {
+        Console.WriteLine("Menghitung keliling bangun datar");
+        return 0;
+    }
+
+}
+
+class Persegi : BangunDatar
+{
+    public float sisi;
+    public Persegi(float sisi)
+    {
+        this.sisi = sisi;
+    }
+    public override float luas()
+    {
+        return this.sisi * this.sisi;
+    }
+    public override float keliling()
+    {
+        return this.sisi * 4;
+    }
+}
+
+class Segitiga : BangunDatar
+{
+    public float alas;
+    public float tinggi;
+    public Segitiga(float alas, float tinggi)
+    {
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+    public override float luas()
+    {
+        return this.alas * this.tinggi / 2;
+    }
+}
+class Lingkaran : BangunDatar
+{
+    public float r;
+    public Lingkaran(float r)
+    {
+        this.r = r;
+    }
+    public override float luas()
+    {
+        return (float)(Math.PI * r * r);
+    }
+    public override float keliling()
+    {
+        return (float)(2 * Math.PI * r);
+    }
+}
+
+public class Polymorphism
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Polymorphism");
+        Console.WriteLine("\nPilih operasi bangun datar!");
+        Console.WriteLine("1. Persegi \n2. Segitiga \n3. Lingkaran");
+        int pilih = int.Parse(Console.ReadLine());
+
+        BangunDatar bangunDatar = new BangunDatar();
+
+        bangunDatar.luas();
+        bangunDatar.keliling();
+
+        if (pilih == 1)
+        {
+            Console.WriteLine("\nMasukkan Sisi : ");
+            Persegi persegi = new Persegi(float.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Luas persegi: " + persegi.luas());
+            Console.WriteLine("keliling persegi: " + persegi.keliling());
+        }
+        
+        else if (pilih == 2)
+        {
+            Console.WriteLine("\nMasukkan Alas : ");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Masukkan Tinggi : ");
+            float t = float.Parse(Console.ReadLine());
+            Segitiga segitiga = new Segitiga(a, t);
+
+            Console.WriteLine("Luas segitiga: " + segitiga.luas());
+        }
+
+        else if (pilih == 3)
+        {
+            Console.WriteLine("\nMasukkan Jari-jari : ");
+            Lingkaran lingkaran = new Lingkaran(float.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Luas lingkaran: " + lingkaran.luas());
+            Console.WriteLine("keliling lingkaran: " + lingkaran.keliling());
+        }
+
+        else
+        {
+            Console.WriteLine("\nError!!!");
+        }
+    }
+}
